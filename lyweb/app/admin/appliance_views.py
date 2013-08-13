90c3e08889f81aa9df2ef77724fe0027b6ce0ac6
Jian Lee <lijian.gnu@gmail.com>
2013-08-13 11:34:55 +0800

    1. Update appliance management in admin console 2. Update instance management

--- lyweb/app/admin/appliance_views.py
+++ lyweb/app/admin/appliance_views.py
@@ -8,7 +8,8 @@ from sqlalchemy.sql.expression import asc, desc
 from app.auth.models import User, Group, Permission
 from app.appliance.models import Appliance, ApplianceCatalog
 
-from app.admin.forms import CatalogForm
+from app.admin.forms import ApplianceCatalogEditForm, \
+    ApplianceEditForm
 
 from lycustom import has_permission
 from lytool.filesize import size as human_size
@@ -16,8 +17,372 @@ from ytool.pagination import pagination
 
 from settings import LY_TARGET
 
-class ApplianceManagement(RequestHandler):
 
+
+class Index(RequestHandler):
+
+    @has_permission('admin')
+    def get(self):
+
+        catalog_id = self.get_argument_int('catalog', 0)
+        page_size = self.get_argument_int('sepa', 20)
+        cur_page = self.get_argument_int('p', 1)
+        by = self.get_argument('by', 'id')
+        sort = self.get_argument('sort', 'ASC')
+        uid = self.get_argument('uid', 0)
+
+        if by not in [ 'name', 'created', 'updated', 'id', 'os',
+                       'disksize',
+                       'user_id', 'catalog_id', 'filesize',
+                       'islocked', 'isprivate',
+                       'like', 'unlike', 'visit' ]:
+            by = 'id'
+
+        by_exp = desc(by) if sort == 'DESC' else asc(by)
+        start = (cur_page - 1) * page_size
+        stop = start + page_size
+
+        catalog = self.db.query(ApplianceCatalog).get( catalog_id )
+        user = self.db.query(User).get( uid )
+
+        apps = self.db.query(Appliance)
+
+        if catalog:
+            apps = apps.filter_by(catalog_id=catalog_id)
+
+        if user:
+            apps = apps.filter_by(user_id = uid)
+
+        apps = apps.order_by(by_exp)
+
+        total = apps.count()
+        apps = apps.slice(start, stop)
+            
+        page_html = pagination(self.request.uri, total, page_size, cur_page)
+
+        catalogs = self.db.query(ApplianceCatalog).all()
+        for c in catalogs:
+            c.total = self.db.query(Appliance.id).filter_by( catalog_id = c.id ).count()
+
+        d = { 'title': _('LuoYun Appliance Management'),
+              'BY': by, 'SORT': sort,
+              'CATALOG_LIST': catalogs, 'CATALOG': catalog,
+              'APPLIANCE_LIST': apps, 'PAGE_HTML': page_html,
+              'CATALOG': catalog, 'USER': user,
+              'TOTAL_APPLIANCE': total,
+              'human_size': human_size,
+              'urlupdate': self.urlupdate,
+              'PAGE_SIZE': page_size }
+
+        self.render( 'admin/appliance/index.html', **d )
+
+
+
+class ApplianceView(RequestHandler):
+
+    @has_permission('admin')
+    def get(self):
+
+        ID = self.get_argument_int('id', None)
+        if not ID:
+            return self.write( _('Give me appliance id please.') )
+
+        A = self.db.query(Appliance).get( ID )
+        if not A:
+            return self.write( _('Can not find appliance %s') % ID )
+
+        self.render( 'admin/appliance/view.html',
+                     title = _('View Appliance "%s"') % A.name,
+                     APPLIANCE = A,
+                     human_size = human_size )
+
+
+
+class ApplianceChangeUser(RequestHandler):
+
+    title = _('Change owner of appliance')
+    template_path = 'admin/appliance/change_owner.html'
+
+    @has_permission('admin')
+    def prepare(self):
+
+        ID = self.get_argument_int('id', None)
+        if not ID:
+            return self.finish( _('Give me appliance id please.') )
+
+        A = self.db.query(Appliance).get( ID )
+        if not A:
+            return self.finish( _('Can not find appliance %s') % ID )
+
+        self.prepare_kwargs['APPLIANCE'] = A
+
+
+    def get(self):
+        self.render()
+
+    def post(self):
+
+        A = self.prepare_kwargs['APPLIANCE']
+        E = []
+        U = None
+
+        user = self.get_argument('user', 0)
+        if user:
+            if user.isdigit():
+                U = self.db.query(User).get(user)
+
+            if not U:
+                U = self.db.query(User).filter_by(
+                    username = user ).first()
+
+            if U:
+
+                old_owner = A.user.username
+                new_owner = U.username
+
+                A.user = U
+                self.db.commit()
+
+                # save trace
+                do = _('change owner %(old)s to %(new)s') % {
+                    'old': old_owner, 'new': new_owner }
+
+                reason = self.get_argument('reason', None)
+                if reason:
+                    do += ' : %s' % reason
+
+                T = self.lytrace( ttype = LY_TARGET['APPLIANCE'],
+                                  tid = A.id, do = do )
+
+                # TODO: send reason to user
+                url = self.reverse_url('admin:appliance:view')
+                url += '?id=%s' % A.id
+                return self.redirect( url )
+
+            else:
+                E.append( _('Can not find user %s') % user )
+        else:
+            E.append( _('No user input !') )
+
+        self.render(ERROR = E)
+
+
+
+class ApplianceEdit(RequestHandler):
+
+    title = _('Edit Appliance')
+    template_path = 'admin/appliance/edit.html'
+
+    @has_permission('admin')
+    def prepare(self):
+
+        ID = self.get_argument_int('id', None)
+        if not ID:
+            return self.finish( _('Give me appliance id please.') )
+
+        A = self.db.query(Appliance).get( ID )
+        if not A:
+            return self.finish( _('Can not find appliance %s') % ID )
+
+
+        catalog_choices = []
+        for s in self.db.query(ApplianceCatalog.id,
+                               ApplianceCatalog.name).all():
+            catalog_choices.append( (str(s.id), s.name) )
+
+        form = ApplianceEditForm(self)
+        form.catalog.choices = catalog_choices
+
+        self.prepare_kwargs['APPLIANCE'] = A
+        self.prepare_kwargs['form'] = form
+
+
+    def get(self):
+
+        A = self.prepare_kwargs['APPLIANCE']
+        form = self.prepare_kwargs['form']
+
+        form.catalog.default = A.catalog_id
+        form.os.default = A.os
+        form.process()
+
+        form.name.data = A.name
+        form.summary.data = A.summary
+        form.description.data = A.description
+
+        self.render()
+
+    def post(self):
+
+        A = self.prepare_kwargs['APPLIANCE']
+        form = self.prepare_kwargs['form']
+
+        if form.validate():
+            A.name        = form.name.data
+            A.os          = self.get_int(form.os.data)
+            A.summary     = form.summary.data
+            A.catalog_id  = form.catalog.data
+            A.description = form.description.data
+
+            # Save logo file
+            if self.request.files:
+                r = A.save_logo(self.request.files['logo'])
+                if r:
+                    form.logo.errors.append( r )
+
+            self.db.commit()
+            url = self.reverse_url('admin:appliance:view')
+            url += '?id=%s' % A.id
+            return self.redirect( url )
+
+
+        self.render()
+
+
+
+class CatalogIndex(RequestHandler):
+
+    @has_permission('admin')
+    def get(self):
+
+        CL = self.db.query(ApplianceCatalog).order_by('id').all()
+
+        d = { 'title': _('Appliance Catalog Home'),
+              'CATALOG_LIST': CL }
+
+        self.render( 'admin/appliance/catalog/index.html', **d )
+
+
+
+class CatalogView(RequestHandler):
+
+    @has_permission('admin')
+    def get(self):
+
+        ID = self.get_argument_int('id', None)
+        if not ID:
+            return self.write( _('Give me catalog id please.') )
+
+        C = self.db.query(ApplianceCatalog).get( ID )
+        if not C:
+            return self.write( _('Can not find catalog %s') % ID )
+
+
+        d = { 'title': _('View Appliance Catalog : "%s"') % C.name,
+              'CATALOG': C }
+
+        self.render( 'admin/appliance/catalog/view.html', **d )
+
+
+
+class CatalogAdd(RequestHandler):
+
+    title = _('Add Appliance Catalog')
+    template_path = 'admin/appliance/catalog/add.html'
+
+    @has_permission('admin')
+    def prepare(self):
+        self.prepare_kwargs['form'] = ApplianceCatalogEditForm(self)
+
+    def get(self):
+        self.render()
+
+    def post(self):
+
+        form = self.prepare_kwargs['form']
+
+        if form.validate():
+            C = ApplianceCatalog(
+                name        = form.name.data,
+                summary     = form.summary.data,
+                description = form.description.data )
+            self.db.add( C )
+            self.db.commit()
+
+            url = self.reverse_url('admin:appliance:catalog:view')
+            url += '?id=%s' % C.id
+            return self.redirect( url )
+
+        self.render()
+
+
+class CatalogEdit(RequestHandler):
+
+    title = _('Edit Appliance Catalog')
+    template_path = 'admin/appliance/catalog/edit.html'
+
+    @has_permission('admin')
+    def prepare(self):
+
+        ID = self.get_argument_int('id', None)
+        if not ID:
+            return self.finish( _('Give me catalog id please.') )
+
+        C = self.db.query(ApplianceCatalog).get( ID )
+        if not C:
+            return self.finish( _('Can not find catalog %s') % ID )
+
+        self.prepare_kwargs['form'] = ApplianceCatalogEditForm(self)
+        self.prepare_kwargs['CATALOG'] = C
+
+
+    def get(self):
+
+        C = self.prepare_kwargs['CATALOG']
+        form = self.prepare_kwargs['form']
+
+        form.name.data        = C.name
+        form.summary.data     = C.summary
+        form.description.data = C.description
+
+        self.render()
+
+
+    def post(self):
+
+        C = self.prepare_kwargs['CATALOG']
+        form = self.prepare_kwargs['form']
+
+        if form.validate():
+            C.name        = form.name.data
+            C.summary     = form.summary.data
+            C.description = form.description.data
+            self.db.commit()
+
+            url = self.reverse_url('admin:appliance:catalog:view')
+            url += '?id=%s' % C.id
+            return self.redirect( url )
+
+        self.render()
+
+
+
+class CatalogDelete(RequestHandler):
+
+    @has_permission('admin')
+    def get(self):
+
+        ID = self.get_argument_int('id', None)
+        if not ID:
+            return self.write( _('Give me catalog id please.') )
+
+        C = self.db.query(ApplianceCatalog).get( ID )
+        if not C:
+            return self.write( _('Can not find catalog %s') % ID )
+
+        if len(C.appliances):
+            return self.write( _('There are appliances exist, can not delete catalog now.') )
+
+        self.db.delete( C )
+        self.db.commit()
+
+        url = self.reverse_url('admin:appliance:catalog')
+        self.redirect( url )
+
+
+
+
+class ApplianceManagement(RequestHandler):
 
     @has_permission('admin')
     def prepare(self):
@@ -69,104 +434,6 @@ class ApplianceManagement(RequestHandler):
             self.write( self.trans(_('Wrong action value!')) )
 
 
-    def get_index(self):
-
-        catalog_id = self.catalog.id if self.catalog else 0
-        page_size = self.get_argument_int('sepa', 20)
-        cur_page = self.get_argument_int('p', 1)
-        by = self.get_argument('by', 'id')
-        sort = self.get_argument('sort', 'ASC')
-        uid = self.get_argument('uid', 0)
-
-        by_exp = desc(by) if sort == 'DESC' else asc(by)
-        start = (cur_page - 1) * page_size
-        stop = start + page_size
-
-        catalog = self.db.query(ApplianceCatalog).get( catalog_id )
-        user = self.db.query(User).get( uid )
-
-        apps = self.db.query(Appliance)
-
-        if catalog:
-            apps = apps.filter_by(catalog_id=catalog_id)
-
-        if user:
-            apps = apps.filter_by(user_id = uid)
-
-        apps = apps.order_by(by_exp)
-
-        total = apps.count()
-        apps = apps.slice(start, stop)
-            
-        page_html = pagination(self.request.uri, total, page_size, cur_page)
-
-
-        catalogs = self.db.query(ApplianceCatalog).all()
-        for c in catalogs:
-            c.total = self.db.query(Appliance.id).filter_by( catalog_id = c.id ).count()
-
-        d = { 'title': self.trans(_('LuoYun Appliance Management')),
-              'CATALOG_LIST': catalogs, 'CID': catalog_id,
-              'APPLIANCE_LIST': apps, 'PAGE_HTML': page_html,
-              'CATALOG': catalog, 'USER': user,
-              'TOTAL_APPLIANCE': total,
-              'human_size': human_size,
-              'urlupdate': self.urlupdate,
-              'PAGE_SIZE': page_size }
-
-        self.render( 'admin/appliance/index.html', **d )
-
-
-
-    def get_view(self):
-        catalogs = self.db.query(ApplianceCatalog).all()
-        self.render( 'admin/appliance/view.html',
-                     title = self.trans(_('View Appliance %s')) % self.appliance.name,
-                     CATALOG_LIST = catalogs,
-                     APPLIANCE = self.appliance,
-                     human_size = human_size )
-
-    def change_owner(self):
-        d = { 'title': self.trans(_('Change owner of appliance')),
-              'A': self.appliance }
-        
-        E = []
-        U = None
-        
-        if self.request.method == 'POST':
-            user = self.get_argument('user', 0)
-            if user:
-                if user.isdigit():
-                    U = self.db.query(User).get(user)
-                if not U:
-                    U = self.db.query(User).filter_by(username=user).first()
-                if not U:
-                    E.append( self.trans(_('Can not find user: %s')) % user )
-            else:
-                E.append( self.trans(_('No user input !')) )
-
-            reason = self.get_argument('reason', '')
-
-            if E:
-                d['ERROR'] = E
-            else:
-                T = self.lytrace(
-                    ttype = LY_TARGET['APPLIANCE'],
-                    tid = self.appliance.id,
-                    do = self.trans(_('change appliance owner %(old_owner)s to %(new_owner)s')) % {
-                        'old_owner': self.appliance.user.username, 'new_owner': U.username } )
-
-                self.appliance.user = U
-                self.db.commit()
-
-                # TODO: send reason to user
-                url = self.reverse_url('admin:appliance')
-                url += '?id=%s' % self.appliance.id
-                return self.redirect( url )
-
-        self.render( 'admin/appliance/change_owner.html', **d)
-
-
     def change_catalog(self):
 
         CATALOG_LIST = self.db.query(ApplianceCatalog).all()
@@ -200,108 +467,4 @@ class ApplianceManagement(RequestHandler):
 
 
 
-
-class CatalogManagement(RequestHandler):
-
-    @has_permission('admin')
-    def prepare(self):
-
-        self.action = self.get_argument('action', 'index')
-
-        cid = self.get_argument_int('id', 0)
-        self.catalog = self.db.query(ApplianceCatalog).get(cid)
-
-        if self.action in ['edit'] and not self.catalog:
-            self.write( self.trans(_('No catalog specified !')) )
-            return self.finish()
-
-
-    def get(self):
-
-        if self.action == 'index':
-            self.get_index()
-
-        elif self.action == 'new':
-            self.get_new()
-
-        elif self.action == 'edit':
-            self.get_edit()
-
-        else:
-            self.write( self.trans(_('Wrong action: %s')) % self.action )
-
-
-    def post(self):
-
-        if self.action == 'new':
-            self.post_new()
-
-        elif self.action == 'edit':
-            self.post_edit()
-
-        else:
-            self.write( self.trans(_('Wrong action: %s')) % self.action )
-
-
-    def get_index(self):
-
-        CL = self.db.query(ApplianceCatalog).order_by('id').all()
-
-        d = { 'title': self.trans(_('Appliance Catalog')),
-              'CATALOG_LIST': CL }
-
-        self.render( 'admin/appliance/catalog.html', **d )
-
-
-    def get_new(self):
-        self.render( 'admin/appliance/catalog_new.html',
-                     title = self.trans(_('Add Appliance Catalog')),
-                     form = CatalogForm(self) )
-
-    def post_new(self):
-
-        form = CatalogForm(self)
-        if form.validate():
-            c = ApplianceCatalog(
-                name = form.name.data,
-                summary = form.summary.data,
-                description = form.description.data )
-            self.db.add( c )
-            self.db.commit()
-
-            url = self.reverse_url('admin:appliance:catalog')
-            return self.redirect( url )
-
-        self.render( 'admin/appliance/catalog_new.html',
-                     title = self.trans(_('Add Appliance Catalog')),
-                     form = form )
-
-
-    def get_edit(self):
-
-        form = CatalogForm(self)
-        form.name.data = self.catalog.name
-        form.summary.data = self.catalog.summary
-        form.description.data = self.catalog.description
-
-        self.render( 'admin/appliance/catalog_edit.html',
-                     title = self.trans(_('Edit Appliance Catalog: %s')) % self.catalog.name,
-                     form = form )
-
-
-    def post_edit(self):
-
-        form = CatalogForm(self)
-        if form.validate():
-            self.catalog.name = form.name.data
-            self.catalog.summary = form.summary.data
-            self.catalog.description = form.description.data
-            self.db.commit()
-
-            url = self.reverse_url('admin:appliance:catalog')
-            return self.redirect( url )
-
-        self.render( 'admin/appliance/catalog_edit.html',
-                     title = self.trans(_('Edit Appliance Catalog: %s')) % self.catalog.name,
-                     form = form )
 
